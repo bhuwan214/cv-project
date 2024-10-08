f08025af348bc55bc7093411620dceb2d1f34c95
void-the-droid <bhuwan626282@gmail.com>
2024-10-08 23:43:16 +0545

    first commit

---
+++ cv-project/src/components/Base.jsx
@@ -0,0 +1,20 @@
+export const Base = ()=>{
+    return (<>
+<main>
+  
+    <div className="left-bar">
+        <div className="edit"></div>
+        <div className="description">
+            <div className="personal-detail"></div>
+            <div className="education">
+        
+            </div>
+            <div className="experince"></div>
+        </div>
+    </div>
+    <div className="container"></div>
+    </main>
+
+    </>)
+
+}
